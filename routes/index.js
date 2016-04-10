5b74273a7c0d39f3ca13641a51fd27ba7cf96912
Tal Davidi <tal.davidi@gmail.com>
2016-04-10 02:10:01 -0400

    added login functionality + almost register functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Login' });
+  res.render('index', { title: 'Login'});
 });
 
 router.get('/helloworld', function(req, res) {
@@ -19,6 +20,77 @@ router.get('/userlist', function(req,res) {
 		});
 	});
 });
+
+/* Login a user */
+router.post('/login', function(req,res){
+	var db = req.db;
+	
+	var userName= req.body.loginUsername;
+	var userPassword= req.body.loginPassword;
+	var hash = crypto.createHmac("sha1",'I am a beautiful gopherA98DF23').update(userPassword).digest('hex');
+		
+	var collection = db.get('usercollection').find({'username': {$eq : userName} },{},function(e,docs){
+		
+		var dbHash=docs[0].passwordHash;
+		
+		if(dbHash === hash){
+			
+			res.render('loggedin', {title: 'Logged in Succesfully'});
+			
+		}
+		else{
+			
+			res.render('index', { title: 'Login', failed: 'true' });
+		}
+	});	
+});
+
+router.post('/register', function(req,res){
+	var db = req.db;
+	
+	var firstname=req.body.registerFirstName;
+	var lastname=req.body.registerLastName;
+	var username=req.body.registerUserName;
+	var email=req.body.registerEmail;
+	var password=req.body.registerPassword;
+	var confirm=req.body.registerConfirm;
+	
+	if(password !== confirm){
+		
+		res.render('index', {title: 'Login', registerError: 'Passwords do not match'});
+	}
+	else{
+		
+		var collection = db.get('usercollection').find({ $or: [ {'username': {$eq : username}} , {'email': {$eq : email}} ] },{},function(e,docs){
+			
+			if(docs.length >= 1){
+				
+				if(docs[0].username === username){
+						
+					res.render('index', {title: 'Login', registerError: 'Username already exists'});
+				}
+				else{
+					
+					res.render('index', {title: 'Login', registerError: 'Email already registered'});
+				}
+			}
+			else{
+				
+				//write to the database
+			}
+		});
+		
+		collection = db.get('usercollection').find({'email': {$eq : email}},{},function(e,docs){
+			
+			if(docs.length >= 1){
+				
+				res.render('index', {title: 'Login', registerError: 'Email already exists'});
+			}
+		});
+	
+	}	
+});
+
 
 /* Get new user page */
 router.get('/newuser', function(req,res){
